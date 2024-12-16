9fa02b532fc7ea3009a256f4d13723981182fc8d
Thalita Lima de Lira <20211064010009@ifrn.local>
2024-12-16 13:24:30 -0300

    Projeto Pronto!

---
+++ AtividadePDS/src/App.java
@@ -0,0 +1,14 @@
+
+public class App {
+    public static void main(String[] args) {
+        // Criando uma instância de Pessoa para cada estuantes
+        Pessoa pessoa = new Pessoa("Daniel", 25);
+        Pessoa pessoa1 = new Pessoa("Thalita", 19);
+        // Exibindo as informações dos estudantes
+        pessoa.exibirInformacoes();
+        pessoa1.exibirInformacoes();
+    }
+}
+	
+
+
